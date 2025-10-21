969ee649adcff9623f84657317e9cfdef6565fd8
free-toolkit <saidsahar2013@gmail.com>
2025-10-21 14:57:27 +0200

    Create sw.js

---
+++ demo-restaurante/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'gastroneo-v1';
+const urlsToCache = [
+  'index.html',
+  '../shared/shared.css',
+  '../shared/shared.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;700&family=Rajdhani:wght@400;500;600&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
